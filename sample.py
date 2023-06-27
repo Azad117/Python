7e603278ae9bc6585eaaaad771de1de240c41686
J.VenuGopal Azad <95510274+Azad117@users.noreply.github.com>
2023-06-27 21:12:03 +0530

    sample.py

---
+++ sample.py
@@ -0,0 +1,29 @@
+# Integer
+age = 25
+# Float
+height = 1.75
+# String
+name = "John Doe"
+# Boolean
+is_student = True
+# List
+grades = [80, 90, 75, 85, 95]
+# Tuple
+coordinates = (10, 20)
+# Dictionary
+person = {
+    "name": "Alice",
+    "age": 30,
+    "is_student": False
+}
+# Set
+fruits = {"apple", "banana", "orange"}
+# Print the values and types
+print("Age:", age, type(age))
+print("Height:", height, type(height))
+print("Name:", name, type(name))
+print("Is Student:", is_student, type(is_student))
+print("Grades:", grades, type(grades))
+print("Coordinates:", coordinates, type(coordinates))
+print("Person:", person, type(person))
+print("Fruits:", fruits, type(fruits))
